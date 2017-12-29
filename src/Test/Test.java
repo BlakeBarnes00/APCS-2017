4dd7fb13ed080085ca56d13330da39fc70ad0f08
Blake Barnes <ellisbarnes00@gmail.com>
2017-12-29 00:04:05 -0500

    6.03 exam leftovers

---
+++ src/Test/Test.java
@@ -0,0 +1,15 @@
+package Test;
+
+
+/**
+ *  This class is for testing and running random lines of code into it for exams
+ */
+public class Test {
+    public static void main(String[] args) {
+        int[] arrNum = { 1, 2, 3, 4, 5 };
+
+        for(int a : arrNum) {
+            System.out.println(a);
+        }
+    }
+}
