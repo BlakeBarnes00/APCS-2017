48e128ae889db340098e0e22f9639db5cef5e619
Blake Barnes <ellisbarnes00@gmail.com>
2018-01-03 05:51:11 -0500

    Finished adding catagory detection

--- src/mod6/Assignments/Challenges/HurricaneChallenge.java
+++ src/mod6/Assignments/Challenges/HurricaneChallenge.java
@@ -60,10 +60,25 @@ public class HurricaneChallenge {
     }
 
     static ArrayList<String> format(ArrayList<Integer> year, ArrayList<Integer> pressure, ArrayList<Double> mph, ArrayList<String> name) {
-        ArrayList<String> s = new ArrayList<>();
+        ArrayList<String> s         = new ArrayList<>();
+        ArrayList<Integer> catagory = new ArrayList<>();
+
         DecimalFormat format = new DecimalFormat("#.00");
+        for (double speed : mph) {
+            if (speed >= 74 && speed <= 95)
+                catagory.add(1);
+            else if (speed >= 96 && speed <= 110)
+                catagory.add(2);
+            else if (speed >= 111 && speed <= 129)
+                catagory.add(3);
+            else if (speed >= 130 && speed <= 156)
+                catagory.add(4);
+            else if (speed >= 157)
+                catagory.add(5);
+
+        }
         for (int i = 0; i < year.size(); i++) {
-            s.add(year.get(i) + "\t" + name.get(i) + "\t\t\t\t0" + "\t\t\t" + pressure.get(i) + "\t\t\t\t" + format.format(mph.get(i)));
+            s.add(year.get(i) + "\t" + name.get(i) + "\t\t\t\t"+ catagory.get(i) + "\t\t\t" + pressure.get(i) + "\t\t\t\t" + format.format(mph.get(i)));
         }
         return s;
     }
