f0c6b0b131d5ae3cdbe3d8c27dd4753902721171
Blake Barnes <ellisbarnes00@gmail.com>
2018-01-10 01:20:17 -0500

    Started 07.03

---
+++ src/mod7/Assignments/PlanetWeight.java
@@ -0,0 +1,31 @@
+package mod7.Assignments;
+
+/**
+ * @author Ellis Barnes
+ * @version 12/10/2017
+ * Program to demonstrate the use of static functions
+ */
+public class PlanetWeight {
+    public static void main(String[] args) {
+        String[] planets    = { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune" };
+        double[] gravity    = { 3.70, 8.87, 9.79, 3.70, 24.78, 10.44, 8.86, 11.13 };
+        double[] weight     = { 37.79, 90.60, 100.00, 37.79, 253.12, 106.64, 90.50, 113.69 };
+        double[] mass       = { 3.30e23, 4.869e24, 5.972e24, 6.4219e23, 1.900e27, 5.68e26, 8.683e25, 1.0247e26 };
+
+        print(planets, gravity, weight);
+    }
+
+    public static double calculate(double m, double sg) {
+        return m * sg;
+    }
+
+    public static void print(String[] p, double[] g, double[] m) {
+        System.out.printf("%31s %n", "Planetary Data");
+        System.out.printf("%s %20s %20s %n", "Planet", "Gravity", "Weight (lbs)");
+        System.out.println("------------------------------------------------");
+        for(int i = 0; i < p.length; i++) {
+            double weight = calculate(m[i], g[i]);
+            System.out.printf("%-13s %11.2f %19.2e %n", p[i], g[i], weight);
+        }
+    }
+}
