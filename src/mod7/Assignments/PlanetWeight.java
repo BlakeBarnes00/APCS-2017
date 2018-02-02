2e5d538dc2d0bb85d787fd484106e21d54a53059
Blake Barnes <ellisbarnes00@gmail.com>
2018-02-01 21:54:00 -0500

    Finished 7.04

--- src/mod7/Assignments/PlanetWeight.java
+++ src/mod7/Assignments/PlanetWeight.java
@@ -1,5 +1,7 @@
 package mod7.Assignments;
 
+import java.util.Scanner;
+
 /**
  * @author Ellis Barnes
  * @version 12/10/2017
@@ -9,22 +11,29 @@ public class PlanetWeight {
     public static void main(String[] args) {
         String[] planets    = { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune" };
         double[] gravity    = { 3.70, 8.87, 9.79, 3.70, 24.78, 10.44, 8.86, 11.13 };
-        double[] weight     = { 37.79, 90.60, 100.00, 37.79, 253.12, 106.64, 90.50, 113.69 };
+        //double[] weight     = { 37.79, 90.60, 100.00, 37.79, 253.12, 106.64, 90.50, 113.69 };
 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a weight: ");
+        double weight = sc.nextDouble();
         print(planets, gravity, weight);
     }
 
-    public static double calculate(double m, double sg) {
-        return m * sg;
+    // Convert the weight from LBS to grams
+    public static double toMetric(double weight, double gravity) {
+        // 1 LBS = 433.59237; 9.8 is earths gravity (scales others to the same ratio)
+        double m = weight * (433.59237) / (gravity/9.8);
+        return m;
     }
 
-    public static void print(String[] p, double[] g, double[] m) {
+    public static void print(String[] p, double[] g, double w) {
         System.out.printf("%31s %n", "Planetary Data");
         System.out.printf("%s %20s %20s %n", "Planet", "Gravity", "Weight (lbs)");
         System.out.println("------------------------------------------------");
         for(int i = 0; i < p.length; i++) {
-            double weight = calculate(m[i], g[i]);
-            System.out.printf("%-13s %11.2f %19.2e %n", p[i], g[i], weight);
+            double grams = toMetric(w, g[i]);
+
+            System.out.printf("%-13s %11.2f %19.2e %n", p[i], g[i], grams);
         }
     }
 }
