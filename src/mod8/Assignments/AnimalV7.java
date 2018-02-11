4ec375b888d530b8519e534315f1f22ab688ded5
Ellis Barnes <ellisbarnes00@gmail.com>
2018-02-11 15:29:25 -0500

    Closes #4

--- src/mod8/Assignments/AnimalV7.java
+++ src/mod8/Assignments/AnimalV7.java
@@ -1,4 +1,40 @@
 package mod8.Assignments;
 
+/**
+ * Animal class to show how to use classes to prevent remaking code over again.
+ * @author ellis
+ * @version 02/11/18
+ */
 public class AnimalV7 {
+    private double height, weight;
+
+    public AnimalV7() {
+        this.height = 0;
+        this.weight = 0;
+    }
+
+    public AnimalV7(double height, double weight) {
+        this.height = height;
+        this.weight = weight;
+    }
+
+    // Setters
+    public void setHeight(double height) { this.height = height; }
+    public void setWeight(double weight) { this.weight = weight; }
+
+    // Convert centimeters to inches
+    public double getHeight() {
+        return this.height * 0.393701;
+    }
+
+    // Convert kilograms to pounds
+    public double getWeight() {
+        return this.weight * 2.20462;
+    }
+
+    public static void main(String[] args) {
+        AnimalV3 dog = new AnimalV3(28.3, 32.97);
+        System.out.printf("Height: %3.2f inches. %n", dog.getHeight());
+        System.out.printf("Weight: %3.2f pounds. %n", dog.getWeight());
+    }
 }
--- src/mod8/Assignments/AnimalV7Tester.java
+++ src/mod8/Assignments/AnimalV7Tester.java
@@ -1,4 +1,21 @@
 package mod8.Assignments;
 
+/**
+ * Tester class for AnimalV7.java
+ *
+ * @author ellis
+ * @version 02/11/18
+ */
 public class AnimalV7Tester {
+    public static void main(String[] args) {
+        AnimalV7 dog = new AnimalV7();
+        AnimalV7 cat = new AnimalV7(30, 63);
+
+        dog.setHeight(42);
+        dog.setWeight(96);
+
+        System.out.printf("%s %20s %20s %n", "Animal", "Height", "Weight");
+        System.out.printf("%s %22.2f %21.2f %n", "Dog", dog.getHeight(), dog.getWeight());
+        System.out.printf("%s %22.2f %21.2f %n", "Cat", cat.getHeight(), cat.getWeight());
+    }
 }
